8857bf939171e56c929c9472738edf9b1a5d2dd7
amdegregorio <fake@donotspam.com>
2018-10-29 06:52:21 -0400

    initial commit of skeleton project

---
+++ src/main/java/com/amydegregorio/beaconapi/beaconapidemo/BeaconApiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.amydegregorio.beaconapi.beaconapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaconApiDemoApplication.class, args);
+	}
+}
